397954763edb7793a180aeb2daf0708588f67c91
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-24 16:42:49 +0100

    updated

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap'
 
 function Questions() {
     return (
-        <div className="img-container">
+        <div className="img-container pt-4">
             <img src='https://images.unsplash.com/photo-1429277096327-11ee3b761c93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2048&q=80' alt='' />
             <h1>Questions <br />
                 about <br />
