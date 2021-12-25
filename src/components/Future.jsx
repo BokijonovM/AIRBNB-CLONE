104b0b6b361b3fac9b40228745e434951a113506
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-25 11:55:04 +0100

    color updated

--- src/components/Future.jsx
+++ src/components/Future.jsx
@@ -8,33 +8,33 @@ function Future() {
             <Row className='container row-cont-e '>
                 <Col className='container col-cont-e'>
                     <p className='active pt-3'>Phoenix</p>
-                    <p className='muted' style={{ color: "grey" }}>Arizona</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>Arizona</p>
                     <p className='active pt-3'>San Francisco</p>
-                    <p className='muted' style={{ color: "grey" }}>California</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>California</p>
                     <p className='active pt-3'>Keswick</p>
-                    <p className='muted' style={{ color: "grey" }}>England</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>England</p>
                 </Col>
                 <Col className='container col-cont-e'>
                     <p className='active pt-3'>Phoenix</p>
-                    <p className='muted' style={{ color: "grey" }}>Arizona</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>Arizona</p>
                     <p className='active pt-3'>San Francisco</p>
-                    <p className='muted' style={{ color: "grey" }}>California</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>California</p>
                     <p className='active pt-3'>Keswick</p>
-                    <p className='muted' style={{ color: "grey" }}>England</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>England</p>
                 </Col>
                 <Col className='container col-cont-e'>
                     <p className='active pt-3'>Phoenix</p>
-                    <p className='muted' style={{ color: "grey" }}>Arizona</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>Arizona</p>
                     <p className='active pt-3'>San Francisco</p>
-                    <p className='muted' style={{ color: "grey" }}>California</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>California</p>
                     <p className='active pt-3'>Keswick</p>
-                    <p className='muted' style={{ color: "grey" }}>England</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>England</p>
                 </Col>
                 <Col className='container col-cont-e'>
                     <p className='active pt-3'>Phoenix</p>
-                    <p className='muted' style={{ color: "grey" }}>Arizona</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>Arizona</p>
                     <p className='active pt-3'>San Francisco</p>
-                    <p className='muted' style={{ color: "grey" }}>California</p>
+                    <p className='muted' style={{ color: "rgb(177, 177, 177)" }}>California</p>
                     <h6 style={{ textDecoration: "underline" }}>Show more</h6>
                 </Col>
             </Row>
