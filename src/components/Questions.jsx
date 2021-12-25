b0aad918b958fc3b7586a8398541ecdfcecbacd3
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-25 14:11:30 +0100

    logos added and add some styles to media query

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,7 +5,7 @@ import { Button } from 'react-bootstrap'
 function Questions() {
     return (
         <div className="img-container pt-4">
-            <img src='https://images.unsplash.com/photo-1429277096327-11ee3b761c93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2048&q=80' alt='' />
+            <img src='https://images.unsplash.com/photo-1475070929565-c985b496cb9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' alt='' />
             <h1>Questions <br />
                 about <br />
                 hosting?</h1>
