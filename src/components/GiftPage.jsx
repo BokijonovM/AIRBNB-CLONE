9f0e7dd9f8aedc92ae805bd1dd5b9d53642b5887
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-05 10:56:11 +0100

    updated

--- src/components/GiftPage.jsx
+++ src/components/GiftPage.jsx
@@ -3,6 +3,8 @@ import MyFooter from './MyFooter'
 import logo from './images/logo1.svg'
 import { Button, Navbar } from 'react-bootstrap'
 import logo1 from './images/gift1.png'
+import GiftPageCards from './GiftPageCards'
+
 
 function GiftPage() {
     return (
@@ -26,7 +28,12 @@ function GiftPage() {
             <div className='w-100 d-flex justify-content-center'>
                 <img className='gift-i-card-img' src={logo1} alt=''></img>
             </div>
-            <div>
+            <div className='text-cont-i-2'>
+                <h1 className='text-cont-i-2-2'>Easy to give. Easy to love.</h1>
+                <h1>Never expires.</h1>
+                <GiftPageCards />
+            </div>
+            <div className='mt-4'>
                 <MyFooter />
             </div>
         </>
---
+++ src/components/GiftPageCards.jsx
@@ -0,0 +1,77 @@
+import React, { createContext } from 'react'
+import { Row, Col, Card, Container } from 'react-bootstrap'
+import logo from './images/airbnb1.svg'
+import { Component } from "react";
+
+
+
+class GiftPageCards extends Component {
+    state = {};
+    cities = [
+        {
+            src: "https://images.unsplash.com/photo-1621659518246-54c47e85d496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+        },
+        {
+            src: "https://images.unsplash.com/photo-1604009261133-30a8abf80cad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+        },
+        {
+            src: "https://images.unsplash.com/photo-1621659518246-54c47e85d496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+        },
+        {
+            src: "https://images.unsplash.com/photo-1604009261133-30a8abf80cad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+        },
+        {
+            src: "https://images.unsplash.com/photo-1621659518246-54c47e85d496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+        },
+        {
+            src: "https://images.unsplash.com/photo-1604009261133-30a8abf80cad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+        }
+
+    ];
+    render() {
+        return (
+            <>
+                <Container fluid className="container__deco">
+                    <div className='container px-5'>
+                        <Row xs={1} md={2} lg={3} className="g-4">
+                            {this.cities.map((city, i) => {
+                                return (
+                                    <Col className='p-0'>
+                                        <Card className='m-3 border-0 gift-i-card'>
+                                            <Card.Img className='giftCard-img-i' variant="top" src={city.src} />
+                                            <Card.Body className='gift-i-card-body'>
+                                                <img className='gift-i-card-img2' src={logo} alt=''></img>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                )
+                            })}
+                        </Row>
+                    </div>
+                </Container>
+            </>
+        );
+    }
+}
+export default GiftPageCards;
+
+// function GiftPageCards() {
+//     return (
+//         <div className='container px-5'>
+//             <Row xs={1} md={2} lg={3} className="g-4">
+//                 {Array.from({ length: 6 }).map((_, idx) => (
+//                     <Col className='p-0'>
+//                         <Card className='m-2 border-0 gift-i-card'>
+//                             <Card.Img className='giftCard-img-i' variant="top" src="https://images.unsplash.com/photo-1621659518246-54c47e85d496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" />
+//                             <Card.Body className='gift-i-card-body'>
+//                                 <img className='gift-i-card-img' src={logo} alt=''></img>
+//                             </Card.Body>
+//                         </Card>
+//                     </Col>
+//                 ))}
+//             </Row>
+//         </div>
+//     )
+// }
+
+// export default GiftPageCards
